b26e888a3a71472d7ba74216afc4ca53308cd347
Poloju Ganesh <166226925+PolojuGanesh@users.noreply.github.com>
2026-09-24 18:51:30 +0530

    Fix import order for App component in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import ContextProvider from "./components/context/Context.jsx";
+import App from "./App.jsx";
 import "./index.css";
 
-import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
